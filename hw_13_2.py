55246caeb7bc86efae1399fb6e7f002ccfec4ecd
teah1412 <74874170+teah1412@users.noreply.github.com>
2021-01-10 13:36:51 +0100

    Update hw_13_2.py

--- hw_13_2.py
+++ hw_13_2.py
@@ -17,23 +17,28 @@ attempts = 0
 
 # otvaram dokument... nešto ne funkkcionira kada u dokumentu ima barem jedan rezultat?
 with open("results.txt", "r") as score_file:
-    score_list = json.loads(score_file.read())
+    results = json.loads(score_file.read())
 
-for score_dict in score_list:
-    print(str(attempts.__dict__["attempts"]) + " attempts, player: ", str(score.__dict__["player_name"]) + ", date: " + score.__dict__.get("date"))
+for score_dict in results:
+    print(str(score_dict["attempts"]) + " attempts, player: ", str(score_dict["player_name"]) + ", date: " + score_dict.get("date"))
 
 
-
-# pitam ime, postavljam new score i počinjem igru
+# pitam ime
 name = input("Please state your name for the score list: ")
 
-new_score = Result(attempts=attempts, player_name=name, date=str(datetime.datetime.now()))
-
+# počinjem igru
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
 
     if guess == secret:
+        new_score = Result(attempts=attempts, player_name=name, date=str(datetime.datetime.now()))
+
+        results.append(new_score.__dict__)
+
+        with open("results.txt", "w") as score_file:
+            score_file.write(json.dumps(results))
+
         print("You've guessed it - congratulations! It's number " + str(secret))
         print("Attempts needed: " + str(attempts))
         print("Score stored in score list.")
